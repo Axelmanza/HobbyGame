7ee4384a15e0fee791af3484c2e3bb1dd075b0a1
Axelmanza <manzanares712@gmail.com>
2020-05-20 11:50:28 +0200

    primeros pasos

---
+++ person.ts
@@ -0,0 +1,57 @@
+export class Person{
+    private name: string
+    private nationality: string
+    private profession: string
+    private expYears: number
+    constructor(name: string, nationality: string, profession: string, expYears: number){
+        this.name = name;
+        this.nationality = nationality;
+        this.profession = profession;
+        this.expYears = expYears;
+    }
+    public getName(){
+        return this.name
+    }
+    public getNation(){
+        return this.nationality
+    }
+    public getProf(){
+        return this.profession
+    }
+    public getExp(){
+        this.expYears
+    }
+    public setName(name: string){
+        this.name = name
+    }
+    public setNation(nation: string){
+        this.nationality = nation
+    }
+    public setProf(prof:string){
+        this.profession = prof
+    }
+    public setExp(exp: number){
+        this.expYears = exp
+    }
+    public inDataBase(person: Person){
+        let isIn: boolean = false;
+        if (person.name === this.name && person.nationality === this.nationality 
+            && person.profession === this.profession && person.expYears === this.expYears){
+            isIn = true;
+        }
+        console.log(isIn);
+    }
+    public print(){
+        console.log(`
+        Name: ${this.name}
+        Nationality: ${this.nationality}
+        Profession: ${this.profession}
+        Experience Years: ${this.expYears}
+        `);
+    }
+    public toString(){
+        console.log('' + this.name + '\n' + this.nationality + '\n' + this.profession + '\n' + this.expYears);
+    }
+
+    
+}
---
+++ testPerson.ts
@@ -0,0 +1,14 @@
+ import {Person} from "./person";
+
+let raquel = new Person('Raquel', 'Spanish', 'Tester', 3);
+let prueba = new Person('Raquel', 'Spanish', 'Tester', 3);
+let prueba2 = new Person('Raquel', 'Spanish', 'Tester', 6);
+let alex = new Person('Alex', 'Spanish', 'Developer', 5);
+
+raquel.toString();
+console.log('----');
+raquel.inDataBase(prueba);
+raquel.inDataBase(prueba2);
+console.log('----');
+alex.print();
+
